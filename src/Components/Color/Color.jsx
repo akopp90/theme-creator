b9d313e4b085c06b936e6ab234a23e147895885f
Andreas Kopp <andreas.kopp90@gmail.com>
2024-10-09 16:27:31 +0200

    integrated functionality and state für updating the colors

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,15 +1,21 @@
 import "./Color.css";
 import { useState } from "react";
+import ColorForm from "../ColorForm/ColorForm";
 
-export default function Color({ color, onDeleteColor }) {
+export default function Color({ color, onDeleteColor, onEditColor }) {
   const [confirmDelete, setConfirmDelete] = useState(false);
+  const [editColor, setEditColor] = useState(false);
 
-  const handleClick = (id) => {
+  const handleDelete = (id) => {
     if (!confirmDelete) {
       setConfirmDelete(true);
     } else {
       onDeleteColor(id);
     }
+  };
+  const handleEdit = (id, data) => {
+    onEditColor(id, data);
+    console.log(data);
   };
 
   return (
@@ -26,21 +32,40 @@ export default function Color({ color, onDeleteColor }) {
       <div className="color-card-buttons">
         {confirmDelete && (
           <>
-            <span className="color-card-highlight">Really delete?</span>
+            <span className="color-card-highlight">Really want to delete?</span>
             <button
               type="button"
-              className="color-card-button"
+              className="color-card-button-cancel"
               onClick={() => setConfirmDelete(false)}
             >
               Cancel
             </button>
           </>
         )}
+        {editColor && (
+          <>
+            <ColorForm color={color} onEditColor={handleEdit} />
+            <button
+              type="button"
+              className="color-card-button-cancel"
+              onClick={() => setEditColor(false)}
+            >
+              Cancel
+            </button>
+          </>
+        )}
+
         <button
           className="color-card-button"
-          onClick={() => handleClick(color.id)}
+          onClick={() => handleDelete(color.id)}
         >
           Delete
+        </button>
+        <button
+          className="color-card-button"
+          onClick={() => setEditColor(true)}
+        >
+          Edit
         </button>
       </div>
     </div>
