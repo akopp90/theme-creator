76220d105765b612c7408371f4a329407d5efc29
Andreas Kopp <andreas.kopp90@gmail.com>
2024-10-09 16:38:51 +0200

    save data in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import ColorForm from "./Components/ColorForm/ColorForm";
-import { useState } from "react";
+
 import { uid } from "uid";
 import "./App.css";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [colors, setColors] = useState(initialColors);
+  const [colors, setColors] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
   const color = { role: "Color name", hex: "#ffffff", contrastText: "#000000" };
 
   function handleAddColor(data) {
