00432c28da20e403150a2d23e28c686dd95c6a77
Andreas Kopp <andreas.kopp90@gmail.com>
2024-10-09 11:46:21 +0200

    implemented functionality to add new colors in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
+import { useState } from "react";
+import { uid } from "uid";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(color) {
+    setColors([{ id: uid(), ...color }, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
+      <div>
+        <ColorForm onSubmitColor={handleAddColor} />
+      </div>
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
