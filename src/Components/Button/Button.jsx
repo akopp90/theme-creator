85aa34da24e80f444161e8ed069b2bd8199dd4e4
Andreas Kopp <andreas.kopp90@gmail.com>
2024-10-10 20:43:37 +0200

    fixed landingpage button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,7 +1,14 @@
 import "./Button.css";
 
 const Button = () => {
-  return <button className="cta-button">Get Started!</button>;
+  return (
+    <button
+      className="cta-button"
+      onClick={() => (window.location.href = "/theme-manager")}
+    >
+      Get Started!
+    </button>
+  );
 };
 
 export default Button;
--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -3,24 +3,16 @@ import Button from "../Button/Button";
 
 function LandingPage() {
   return (
-    <div className="">
-      <div className="landing-page">
-        <h1>Welcome to Theme Manager</h1>
-        <p>Create, customize, and manage your color themes with ease.</p>
-        <ul>
-          <li>Create multiple themes</li>
-          <li>Edit and delete themes</li>
-          <li>Switch between themes instantly</li>
-        </ul>
+    <div className="landing-page">
+      <h1>Welcome to Theme Manager</h1>
+      <p>Create, customize, and manage your color themes with ease.</p>
+      <ul>
+        <li>Create multiple themes</li>
+        <li>Edit and delete themes</li>
+        <li>Switch between themes instantly</li>
+      </ul>
 
-        <Button
-          className="cta-button"
-          onClick={() => (window.location.href = "/theme-manager")}
-          text="Get Started"
-        >
-          Get Started
-        </Button>
-      </div>
+      <Button className="cta-button" text="Get Started" />
     </div>
   );
 }
